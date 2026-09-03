fd7a95cc64967da58335dc03d6c719b2e32f353d
Shubham Shrivastav <ssk06112000@gmail.com>
2026-09-03 13:46:01 +0530

    Refactor sellerController.js and authSeller.js for improved readability and consistency in error handling

--- server/middlewares/authSeller.js
+++ server/middlewares/authSeller.js
@@ -1,22 +1,61 @@
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 
+
+// const authSeller = async (req, res, next) => {
+//     const {sellerToken} = req.cookies;
+
+//     if(!sellerToken){
+//         return res.json({succes: false, message: "Not Authorized"});
+//     }
+//     try {
+//         const tokenDecode = jwt.verify(sellerToken, process.env.JWT_SECRET);
+//         if(tokenDecode.email === process.env.SELLER_EMAIL){
+//             next();
+//         }else{
+//             return res.json({success: false, message: "Not Authorized"});
+//         }
+//     } catch (error) {
+//         res.json({success: false, message: error.message});
+//     }
+// }
+
+// export default authSeller;
+
+
+
+
+import jwt from "jsonwebtoken";
 
 const authSeller = async (req, res, next) => {
-    const {sellerToken} = req.cookies;
+  const { sellerToken } = req.cookies;
 
-    if(!sellerToken){
-        return res.json({succes: false, message: "Not Authorized"});
+  if (!sellerToken) {
+    return res.json({
+      success: false,
+      message: "Not Authorized",
+    });
+  }
+
+  try {
+    const tokenDecode = jwt.verify(
+      sellerToken,
+      process.env.JWT_SECRET
+    );
+
+    if (tokenDecode.email === process.env.SELLER_EMAIL) {
+      next();
+    } else {
+      return res.json({
+        success: false,
+        message: "Not Authorized",
+      });
     }
-    try {
-        const tokenDecode = jwt.verify(sellerToken, process.env.JWT_SECRET);
-        if(tokenDecode.email === process.env.SELLER_EMAIL){
-            next();
-        }else{
-            return res.json({success: false, message: "Not Authorized"});
-        }
-    } catch (error) {
-        res.json({success: false, message: error.message});
-    }
-}
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 export default authSeller;
